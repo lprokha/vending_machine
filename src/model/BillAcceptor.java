ce144296b1ed0ca315423cdcb2bd0b6e771d8c16
Leyla P. <liya1998@list.ru>
2025-12-11 15:44:55 +0600

    Added override to accessPayment method in BillAcceptor.java

--- src/model/BillAcceptor.java
+++ src/model/BillAcceptor.java
@@ -1,4 +1,33 @@
 package model;
 
-public class BillAcceptor {
+import exceptions.BillValueException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class BillAcceptor implements PaymentDevice {
+    @Override
+    public int acceptPayment() {
+        System.out.println("Принимаются купюры: 20, 50, 100, 200, 500, 1000");
+        int billValue;
+        int[] permittedValues = {20, 50, 100, 200, 500, 1000};
+        while (true) {
+            System.out.print("Введите номинал купюры: ");
+            try {
+                billValue = new Scanner(System.in).nextInt();
+
+                for (int p : permittedValues) {
+                    if (billValue == p) {
+                        return billValue;
+                    }
+                }
+                throw new BillValueException("Ошибка: неизвестная купюра");
+            } catch (InputMismatchException e) {
+                System.out.println("Ошибка: введите число");
+            } catch (BillValueException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
 }
