65f772a34e3ba3eb77e0f71e4e8b7d14c11268d7
Leyla P. <liya1998@list.ru>
2025-12-11 17:02:10 +0600

    Modified method acceptPayment in BillAcceptor.java

--- src/model/BillAcceptor.java
+++ src/model/BillAcceptor.java
@@ -7,18 +7,19 @@ import java.util.Scanner;
 
 public class BillAcceptor implements PaymentDevice {
     private Scanner sc = new Scanner(System.in);
+    private int[] bills = {20, 50, 100, 200, 500, 1000};
     @Override
     public int acceptPayment() {
         System.out.println("Принимаются купюры: 20, 50, 100, 200, 500, 1000");
         int billValue;
-        int[] permittedValues = {20, 50, 100, 200, 500, 1000};
+
         while (true) {
             System.out.print("Введите номинал купюры: ");
             try {
                 billValue = sc.nextInt();
 
-                for (int p : permittedValues) {
-                    if (billValue == p) {
+                for (int b : bills) {
+                    if (billValue == b) {
                         return billValue;
                     }
                 }
@@ -30,6 +31,5 @@ public class BillAcceptor implements PaymentDevice {
                 System.out.println(e.getMessage());
             }
         }
-
     }
 }
